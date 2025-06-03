7bedb6d786921b9b91fa8a9d49a659cafcfc2b45
PHnsilva <pedro.vargas@sga.pucminas.br>
2025-06-03 20:55:10 -0300

    chore: melhorias estéticas na homepage

--- Codigo/reparos/frontend/src/pages/Home.jsx
+++ Codigo/reparos/frontend/src/pages/Home.jsx
@@ -11,10 +11,6 @@ const Home = () => {
 
   const servicos = [
     {
-      titulo: "Manutenção de Imóveis",
-      descricao: "Serviços preventivos e corretivos para manter seu imóvel em ótimas condições.",
-    },
-    {
       titulo: "Pequenos Reparos",
       descricao: "Consertos rápidos em portas, janelas, móveis, entre outros.",
     },
@@ -27,16 +23,8 @@ const Home = () => {
       descricao: "Montagem com cuidado e precisão conforme o manual do fabricante.",
     },
     {
-      titulo: "Serviços Hidráulicos",
-      descricao: "Reparos e instalações em torneiras, chuveiros, encanamentos e registros.",
-    },
-    {
       titulo: "Serviços Elétricos",
       descricao: "Instalações e trocas de tomadas, disjuntores e luminárias.",
-    },
-    {
-      titulo: "Pintura",
-      descricao: "Pintura profissional para interiores e exteriores com ótimo acabamento.",
     },
   ];
 
@@ -63,18 +51,24 @@ const Home = () => {
             atualizar seu perfil e muito mais!
           </p>
         </div>
-
-        <div className="servicos-section">
-          <h2 className="servicos-titulo">Nossos Serviços</h2>
-          <div className="servicos-grid">
-            {servicos.map((servico, index) => (
-              <div key={index} className="servico-card">
-                <h3>{servico.titulo}</h3>
-                <p>{servico.descricao}</p>
-              </div>
-            ))}
+        <div className="nossos-servicos"><h2 className="titulo-principal">Nossos Serviços</h2>
+        <button>Ver todos</button></div>
+        <div className="servicos-carrossel-container">
+          <div className="imagem-gelson">
+            <img src="./src/assets/gelson.jpeg" alt="Gelson" />
+          </div>
+          <div className="carrossel-servicos">
+            <div className="carrossel-lista">
+              {servicos.map((servico, index) => (
+                <div key={index} className="carrossel-item">
+                  <h3>{servico.titulo}</h3>
+                  <p>{servico.descricao}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
+
       </main>
     </div>
   );
