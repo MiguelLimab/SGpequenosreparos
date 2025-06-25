700c45a55858bb52cd9c028ed9a63f752a6d2cde
FelipeParreiras <felipe.parreiras04@gmail.com>
2025-06-25 13:51:47 -0300

    config security e api

--- Codigo/frontend/src/services/api.js
+++ Codigo/frontend/src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8081/api',
+  baseURL: 'https://sg-pequenos-reparos-backend.onrender.com/api',
   withCredentials: true,
 });
+
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
